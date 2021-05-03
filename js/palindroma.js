85274c6f0ca45ebf1a8b35294a4cc7b9a93026e9
Guido-95 <guidocarna1995@gmail.com>
2021-05-03 15:22:22 +0200

    esercizio parola palindroma

---
+++ js/palindroma.js
@@ -0,0 +1,40 @@
+// 1 - Palindroma
+// Chiedere all'utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+// funzione controllo parola palindroma
+function controlloPalindroma(parolaDaControllare){
+
+    // parola divisa in lettere in un array restituito come stringa 
+    var parolaDivisa = parolaDaControllare.split("").join();
+    // controllo parola divisa
+    console.log("parola divisa in lettere per confronto: ",parolaDivisa);
+
+    // parola invertita divisa in lettere in un array restituito come stringa
+    var parolaInvertita = parolaInserita.split("").reverse().join();
+    // controllo parola invertita
+    console.log("parola invertita divisa in lettere per confronto: ",parolaInvertita);
+    // se la parola inserità è uguale a sè stessa invertita come ordine, è palindroma
+    if(parolaDivisa == parolaInvertita){
+
+        console.log("è palindroma");
+
+    } else {
+
+        console.log("non è palindroma");
+
+    }
+
+}
+
+// parola richiesta
+var parolaInserita = prompt("inserisci una parola");
+console.log("parola inserita: ",parolaInserita);
+
+controlloPalindroma(parolaInserita);
+
+
+
+
+
